362bd458400541fe86691ea277c6c01d11f3f9d4
Rekvik <vikrek01@gmail.com>
2023-04-20 11:59:24 +0530

    Add project files.

---
+++ BlogApplication_Vikrant/Migrations/202304170547365_eigth.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogApplication_Vikrant.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class eigth : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
